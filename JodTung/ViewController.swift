3d420a0544b5a4bac8c7205c63de35379f21d7ce
Poldet Assanangkornchai <poladet.a@gmail.com>
2016-10-16 16:07:28 +0700

    Initial Commit

---
+++ JodTung/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JodTung
+//
+//  Created by Poldet Assanangkornchai on 10/16/2559 BE.
+//  Copyright © 2559 Poldet Assanangkornchai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
